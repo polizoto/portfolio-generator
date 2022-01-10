791e23d95f3401d7a1ffa493b60e1db708dc1f28
polizoto <polizoto@hotmail.com>
2022-01-10 17:39:33 -0500

    Modularized and refactored code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
+const generatePage = require('./src/page-template.js');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-  printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
