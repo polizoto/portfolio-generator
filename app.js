3196664711d8bbee75595bc51065c77fdbc947e0
polizoto <polizoto@hotmail.com>
2022-01-12 11:12:48 -0500

    Added support for posting data to the HTML page

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
 
@@ -148,4 +140,12 @@ Add a New Project
   });
 };
 
-promptUser().then(promptProject).then(portfolioData => {console.log(portfolioData);});
+promptUser().then(promptProject).then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
